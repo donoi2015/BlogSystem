0e7bc392c283fda21d40ec62cd43b8d9e8e91c13
Tuluvsaikhan <tuluvsaikhan@gmail.com>
2017-06-09 21:24:01 +0800

    Confirmation popup for Post delete

--- src/main/resources/static/js/main.js
+++ src/main/resources/static/js/main.js
@@ -1,6 +1,16 @@
-$(function() {
-    $('#navbar').find('li a').click(function() {
+
+$(document).ready(function(e) {
+    // your code here
+    $('#navbar').find('li a').click(function () {
         $('#navbar').find('li').removeClass();
         $($(this).attr('href')).addClass('active');
     });
 });
+
+function goToURL(x) {
+    bootbox.confirm("Are you sure?", function(result) {
+        if(result) {
+            location.href = '/post/delete/' + x;
+        }
+    });
+}
